efe6199ef56eb4f36145c11938e53b2cce88e857
Amardeep Khera <amardeep.khera@sandstone.com.au>
2018-05-11 12:11:49 +1000

    jwt with client credentials working

---
+++ src/main/kotlin/server/Controller.kt
@@ -0,0 +1,17 @@
+package server
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/test")
+class MyController {
+
+    @GetMapping
+    fun endpoint() = ResponseEntity.ok("hello")
+
+
+}
+
--- src/main/kotlin/server/OAuth2Server.kt
+++ src/main/kotlin/server/OAuth2Server.kt
@@ -2,15 +2,23 @@ package server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore
+
 
 @SpringBootApplication
 @EnableAuthorizationServer
 class OAuth2Server {
-
 
 
 }
@@ -18,16 +26,52 @@ class OAuth2Server {
 @Configuration
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
 
 
-    @Throws(Exception::class)
-    override fun configure(http: HttpSecurity) {
-        http
-                .authorizeRequests()
-                .anyRequest()
-                .permitAll()
+
+
+}
+
+@Configuration
+class OAuth2ServerConfig : AuthorizationServerConfigurer {
+
+
+    override fun configure(authorizationServerSecurityConfigurer: AuthorizationServerSecurityConfigurer) {
+
+    }
+
+    override fun configure(clientDetailsServiceConfigurer: ClientDetailsServiceConfigurer) {
+        clientDetailsServiceConfigurer
+                .inMemory()
+                .withClient("1233")
+                //raw secret is abcc
+                .secret("\$2a\$10\$6AkSrzu8lYix1anvSM0/turYR7eW1ZBc6NbNvgS3SQFfzDF8u9lu6")
+                .authorizedGrantTypes("client_credentials")
+                .scopes("read")
+                .resourceIds("authorization-server")
+
+    }
+
+    override fun configure(authorizationServerEndpointsConfigurer: AuthorizationServerEndpointsConfigurer) {
+
+        authorizationServerEndpointsConfigurer
+                .tokenStore(jwtTokenStore())
+                .accessTokenConverter(jwtAccessTokenConverter())
+    }
+
+    @Bean
+    fun jwtTokenStore() = JwtTokenStore(jwtAccessTokenConverter())
+
+    @Bean
+    fun jwtAccessTokenConverter() = JwtAccessTokenConverter().also {
+        it.setSigningKey("qwerty")
     }
 }
+
 
 fun main(args: Array<String>) {
     runApplication<OAuth2Server>(*args)
